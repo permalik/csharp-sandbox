d45f0d33483f0781703e650ee280d529a5a51683
permalik <permalik@protonmail.com>
2025-03-21 14:46:41 -0500

    feat: impl fibonacci

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 0; i < 10; i++)
+{
+    Console.Write($"{Calc.Fibonacci.v1(i)} ");
+}
+Console.Write("\n");
